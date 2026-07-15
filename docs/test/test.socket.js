47a7ee2dfaa9140a621d0d2a25d5dcea41ad28c4
ClaudineRas02 <hanitraclaudine@gmail.com>
2026-07-15 17:21:40 +0300

    docs: openapi and asyncapi with test file

---
+++ docs/test/test.socket.js
@@ -0,0 +1,43 @@
+// test-socket.js
+
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:3000");
+
+socket.on("connect", () => {
+  console.log("Connecté :", socket.id);
+    socket.emit("join-game", 7);});
+
+socket.on("disconnect", () => {
+  console.log("Déconnecté :", socket.id);
+});
+
+socket.on("created-game", (data) => {
+  console.log("game créée :", data);
+});
+
+socket.on("game:created", (game) => {
+  console.log("Nouvelle partie");
+  console.log(game);
+});
+
+socket.on("game:started", (game) => {
+  console.log("Partie démarrée");
+  console.log(game);
+});
+
+//game:participant-joined
+socket.on("game:participant-joined", (data) => {
+  console.log("Nouveau participant");
+  console.log(data);
+});
+
+socket.on("question:opened", (data) => {
+  console.log("Question ouverte");
+  console.log(data);
+});
+
+socket.on("game:ended", (data) => {
+  console.log("Partie terminée");
+  console.log(data);
+});
